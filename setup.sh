196f16ff21e9f04b282faf6bf13a199631d88eed
Ian <inleuty@gmail.com>
2025-07-23 08:51:47 -0700

    setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# install vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# copy into correct location
+cp ./.bash_aliases ~/
+cp ./.bashrc ~/
+cp ./.vimrc ~/
+cp ./.tmux.conf ~/
+cp -r .tmux ~/
